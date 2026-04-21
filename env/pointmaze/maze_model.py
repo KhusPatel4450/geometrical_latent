1e3871906a2b67b2f62e63f459aca66c1405ee11
Khush <patelkhush433@gmail.com>
2026-04-20 21:35:48 -0400

    EGL fix for rendering

--- env/pointmaze/maze_model.py
+++ env/pointmaze/maze_model.py
@@ -1,6 +1,8 @@
 """ A pointmass maze env."""
+import os
 import sys
 from unittest.mock import MagicMock
+os.environ.setdefault('MUJOCO_GL', 'egl')  # headless offscreen rendering via EGL
 # mujoco_py doesn't compile on Python 3.12; mock it before d4rl imports it
 if 'mujoco_py' not in sys.modules:
     sys.modules['mujoco_py'] = MagicMock()
