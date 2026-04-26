7b6e3ece87ecd54219589929af5df1166afa9695
Khush <patelkhush433@gmail.com>
2026-04-26 19:00:52 -0400

    gradient checkpointing fix for plan_gd

--- planning/gd.py
+++ planning/gd.py
@@ -122,7 +122,6 @@ class GDPlanner(BasePlanner):
             i_z_obses, i_zs = self.wm.rollout(
                 obs_0=trans_obs_0,
                 act=actions,
-                use_grad_checkpoint=True,
             )
             loss = self.objective_fn(i_z_obses, z_obs_g, step=step)  # (n_evals, )
             total_loss = loss.mean() * n_evals  # loss for each eval is independent
